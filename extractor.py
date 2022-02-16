a5e1e67bf02302d85ff1238b4daece5c86f525db
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2022-02-16 23:22:48 +0000

    Added extension to code adds suffixes
    it adds suffixes to the test cases array relating to the choice of the user, this means that no words can be missed (the txt files for nouns and verbs may be missing contextual nouns/verbs)

--- extractor.py
+++ extractor.py
@@ -1,6 +1,3 @@
-
-
-
 def clean_arr(word_array):
 
     arr = word_array
@@ -16,7 +13,11 @@ def main():
     PATH = input("Please input the relative file path:\t")
     x = int(input("What words do you want to extract from the file?\n1.Nouns\n2.Verbs\n\nPick one:\t"))
     words = ""
-
+    # taken from https://www.thoughtco.com/common-suffixes-in-english-1692725
+    suffixes_noun = ["acy","al","ance","ence","dom","er","or","ism","ist","ity","ty","ment","ness","ship","sion","tion"]
+    suffixes_verb = ["ate","en","ify","fy","ize","ise"]
+    # taken from https://www.thoughtco.com/common-suffixes-in-english-1692725        
+     
 
 
     if x == 1:
@@ -24,10 +25,21 @@ def main():
             
             test_cases = file.readlines()
             test_cases = clean_arr(test_cases)
+            curr_length = len(test_cases)
+
+            for i in range(curr_length):
+                for j in range(len(suffixes_noun)):
+                    test_cases.append(test_cases[i] + suffixes_noun[j])
+
     else:
         with open("verbs.txt","r") as file:
             test_cases = file.readlines()
             test_cases = clean_arr(test_cases)
+            curr_length = len(test_cases)
+
+            for i in range(curr_length):
+                for j in range(len(suffixes_verb)):
+                    test_cases.append(test_cases[i] + suffixes_verb[j])
 
     with open(PATH,"r") as file:
 
@@ -44,16 +56,9 @@ def main():
         words = words.split()
         #print(words)
         extraction = [[i for i in words if i in test_cases]]
-        print(extraction)
-        
-
-        
-        
-            
-        
-
-
-
+        print("Words found:\n")
+        for i in range(len(extraction)):
+            print(extraction[i])
 main()
 
     
